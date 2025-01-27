bec061b23dfb013c35a77929dba18f0cac126325
Acpear <2358240334@qq.com>
2025-01-27 14:27:29 +0800

    优化命名

--- src/api/key.rs
+++ src/api/key.rs
@@ -3,12 +3,12 @@ use serde::Deserialize;
 use uuid::Uuid;
 
 #[derive(Deserialize)]
-pub struct CheckCollisionParam {
-    check_collision: Option<bool>,
+pub struct GenKeyParam {
+    v7: Option<bool>, // UUIDv7 单向增加，防止碰撞
 }
 
-pub async fn gen_key(Query(param): Query<CheckCollisionParam>) -> String {
-    let uuid = match param.check_collision {
+pub async fn gen_key(Query(param): Query<GenKeyParam>) -> String {
+    let uuid = match param.v7 {
         None => Uuid::new_v4(),
         Some(false) => Uuid::new_v4(),
         Some(true) => Uuid::now_v7(),
